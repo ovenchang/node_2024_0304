b02c61bf8ebcbca1ee17770b7966af8e84035d78
ovenchang <ovenchang@gmail.com>
2024-03-17 18:04:15 +0800

    k

--- 094_nvm.js
+++ 094_nvm.js
@@ -10,4 +10,21 @@ nvm install 18.12.1 下載
 nvm uninstall 18.12.1 刪除
 nvm use 18.12.1 切換到這個版本
 
+/*********安裝nvm linux */
+1.更新相依套件
+apt-get update
+apt-get install build-essential libssl-dev
+
+2. 前往 https://github.com/nvm-sh/nvm/releases 確認目前nvm最新版本
+
+3. 根據最新版本資訊 使用curl抓取指行語法
+curl https://raw.githubusercontent.com/creationix/nvm/v0.25.0/install.sh | bash
+上面的版本請依照github看到的最新版本進行調整
+
+4. 使用下列指令，自動重開連線
+source ~/.profile
+
+5. 驗證是否成功
+nvm --version
+
 

3fe31de8086457fa5426025dc9b386f8af41cd61
ovenchang <ovenchang@gmail.com>
2024-03-18 16:22:36 +0800

    s

--- 119_express_generator/routes/accounts.js
+++ 119_express_generator/routes/accounts.js
@@ -1,6 +1,19 @@
 const express = require('express')
 const router = express.Router()
 
+//導入 shortid
+const shortid=require('shortid')
+
+//導入lowdb
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+
+const adapter = new FileSync(__dirname+'/../data/db.json')
+//手動加入{account:[]}
+
+//獲取db對象
+const db = low(adapter)
+
 /* 記帳列表頁面 */
 router.get('/', function (req, res, next) {
   res.render('accounts/list')
@@ -13,7 +26,9 @@ router.get('/create', function (req, res, next) {
 
 /**新增紀錄 */
 router.post('/', function (req, res, next) {
-  console.log(req.body)
-  res.send('post')
+  //使用shortid來產生id npm i shortid
+  let id=shortid.generate()
+  db.get('accounts').unshift({id:id, ...req.body}).write()
+  res.render('accounts/success',{msg:'成功'})
 });
 module.exports = router;

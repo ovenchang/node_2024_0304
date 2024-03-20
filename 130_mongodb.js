e5746ffe194a29186ff98aef1e6b57413c805ca8
ovenchang <ovenchang@gmail.com>
2024-03-20 17:37:16 +0800

    輝

--- 130_mongodb.js
+++ 130_mongodb.js
@@ -1,41 +1,57 @@
-/*****lowdb */
-//用於 Node、Electron 和瀏覽器的小型 JSON 資料庫。由lodash提供支援
-//安裝 npm i lowdb@1.0.0
-//您可以使用任何 lodash 函數，例如 _.get 和 _.find 以及簡寫語法。
+/*****mongodb**/
+//核心
+//數據庫database 是數據倉庫 數據庫服務下可以創建許多數據庫 數據庫中可以有很多集合
+//集合collection 類似js中的陣列 可以存放許多文檔
+//文檔 document 數據庫中最小單位 類似js中的物件
 
-//導入
-const low = require('lowdb')
-const FileSync = require('lowdb/adapters/FileSync')
- 
-const adapter = new FileSync('db.json')
+//例如一個json文件就是一個數據庫
+數據庫裡
+{
+    user:[ //集合
+        {name:"lee",age:10}, //文檔 文檔中的屬性有時也稱為字段
+        {name:"lee",age:10}
+    ]
+}
+另一個數據庫
+{
+....
+}
 
-//獲取db對象
-const db = low(adapter)
- 
-// 初始化數據
-//db.defaults({ posts: [], user: {} }).write()
-
-
- 
-// 寫入數據
-// db.get('posts') //取得物件路徑處的值 
-//   .push({ id: 1, title: 'lrrrrrrrr'}).write()
-//  return
-
-//獲取數據
-console.log(db.get('posts').value())
-console.log(db.get('posts').value({id:1}))
-
-//獲取單條數據
-console.log(db.get('posts').find({id:1}).value())
+一般情況下  一個項目使用一個數據庫
+一個集合會存儲同一類型的數據
 
 
-//del數據
-//console.log(db.get('posts').remove({id:1}).write()) //返回del對象
+//MongoDB Atlas (免費)雲端託管服務
+https://ithelp.ithome.com.tw/articles/10268405
 
-//update
-db.get('posts').find({id:1}).assign({title:"55555555"}).write()
 
-// Set a user using Lodash shorthand syntax
-db.set('user.name', 'typicode')
-  .write()
+ovenchang/aSFPmHAwMHpJsvK4
+
+/*****mongoose */
+對象文檔模型庫
+
+/**
+ * const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://ovenchang:aSFPmHAwMHpJsvK4@cluster0.0qefrll.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+ */

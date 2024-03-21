21dfe805bb053e5f0215c8c76785ea23d25ed157
ovenchang <ovenchang@gmail.com>
2024-03-21 09:28:49 +0800

    d

--- 130_mongodb/02_mongoose.js
+++ 130_mongodb/02_mongoose.js
@@ -1,34 +1,32 @@
-/*****mongodb**/
-//核心
-//數據庫database 是數據倉庫 數據庫服務下可以創建許多數據庫 數據庫中可以有很多集合
-//集合collection 類似js中的陣列 可以存放許多文檔
-//文檔 document 數據庫中最小單位 類似js中的物件
-
-//例如一個json文件就是一個數據庫
-數據庫裡
-{
-    user:[ //集合
-        {name:"lee",age:10}, //文檔 文檔中的屬性有時也稱為字段
-        {name:"lee",age:10}
-    ]
-}
-另一個數據庫
-{
-....
-}
-
-一般情況下  一個項目使用一個數據庫
-一個集合會存儲同一類型的數據
-
-
-//MongoDB Atlas (免費)雲端託管服務
-https://ithelp.ithome.com.tw/articles/10268405
-
-
-ovenchang/aSFPmHAwMHpJsvK4
-
 /*****mongoose */
-對象文檔模型庫
+//對象文檔模型庫
+//方便操作mongodb數據庫
+//安裝mongoose
+//npm init
+//npm i
+
+const mongoose= require('mongoose')
+
+//連接mongodb
+mongoose.connect("mongodb+srv://ovenchang:aSFPmHAwMHpJsvK4@cluster0.0qefrll.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+
+//設置callback
+//設置連接成功的callback
+mongoose.connection.on('open',()=>{
+console.log('connect success')
+}) 
+//設置連接錯誤的callback
+mongoose.connection.on('error',()=>{
+  console.log('connect error')
+}) 
+//設置連接關閉的callback
+mongoose.connection.on('close',()=>{
+  console.log('connect close')
+}) 
+
+
+
+
 
 /**
  * const { MongoClient, ServerApiVersion } = require('mongodb');
